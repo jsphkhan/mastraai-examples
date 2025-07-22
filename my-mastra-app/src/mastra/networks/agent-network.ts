933920b7c0d66f8a6af245b004a76fcecd9e951c
Joseph Khan <jsphkhan@gmail.com>
2025-07-22 15:07:16 +0400

    adding more agents and workflows to network

--- my-mastra-app/src/mastra/networks/agent-network.ts
+++ my-mastra-app/src/mastra/networks/agent-network.ts
@@ -5,7 +5,8 @@ import { Memory } from '@mastra/memory';
 import { openai } from '@ai-sdk/openai';
 import { LibSQLStore } from '@mastra/libsql';
 import { z } from 'zod';
-import { RuntimeContext } from '@mastra/core/runtime-context';
+import { weatherAgent } from '../agents/weather-agent';
+import { conditionalWorkflow } from '../workflows/conditional-workflow';
 
 
 const memory = new Memory({
@@ -97,15 +98,17 @@ const workflow1 = createWorkflow({
 const network = new NewAgentNetwork({
     id: 'test-network',
     name: 'Test Network',
-    instructions:
-        'You can research cities. You can also synthesize research material. You can also write a full report based on the researched material. If a city is not found, you should politely say that you research only about cities.',
+    instructions: `You are a network of helpful agents that can answer questions and help with relevant information.`,
+    //'You can research cities. You can also synthesize research material. You can also write a full report based on the researched material. If a city is not found, you should politely say that you research only about cities.',
     model: openai('gpt-4o'),
     agents: {
         agent1,
         agent2,
+        weatherAgent
     },
     workflows: {
         workflow1,
+        conditionalWorkflow
     },
     memory: memory,
 });
